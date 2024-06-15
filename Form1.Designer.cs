7f3f19cb1f2ac207bd5740c0bf52aff53b4605b5
smartkodian <56184963+smartkodian@users.noreply.github.com>
2024-06-15 20:06:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace SSH_Window
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.txtCommands = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtEnablePassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtSwitchIPs = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(324, 302);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(260, 36);
+            this.btnExecute.TabIndex = 5;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // txtCommands
+            // 
+            this.txtCommands.Location = new System.Drawing.Point(12, 196);
+            this.txtCommands.Multiline = true;
+            this.txtCommands.Name = "txtCommands";
+            this.txtCommands.Size = new System.Drawing.Size(934, 100);
+            this.txtCommands.TabIndex = 4;
+            this.txtCommands.Text = resources.GetString("txtCommands.Text");
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(324, 144);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(260, 22);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Text = "Admin@Manager#2050";
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtEnablePassword
+            // 
+            this.txtEnablePassword.Location = new System.Drawing.Point(324, 170);
+            this.txtEnablePassword.Name = "txtEnablePassword";
+            this.txtEnablePassword.Size = new System.Drawing.Size(260, 22);
+            this.txtEnablePassword.TabIndex = 3;
+            this.txtEnablePassword.UseSystemPasswordChar = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(324, 118);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(260, 22);
+            this.txtUsername.TabIndex = 1;
+            this.txtUsername.Text = "super";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 345);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(942, 208);
+            this.txtOutput.TabIndex = 6;
+            // 
+            // txtSwitchIPs
+            // 
+            this.txtSwitchIPs.Location = new System.Drawing.Point(324, 12);
+            this.txtSwitchIPs.Multiline = true;
+            this.txtSwitchIPs.Name = "txtSwitchIPs";
+            this.txtSwitchIPs.Size = new System.Drawing.Size(260, 100);
+            this.txtSwitchIPs.TabIndex = 0;
+            this.txtSwitchIPs.Text = "Enter switch IPs, one per line";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Enter IP Addresses:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "SSH Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(179, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "SSH Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(179, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Enable Password:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Commands:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 321);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Outputs:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 564);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSwitchIPs);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtEnablePassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtCommands);
+            this.Controls.Add(this.btnExecute);
+            this.Name = "Form1";
+            this.Text = "Remote Access Network ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtSwitchIPs;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtEnablePassword;
+        private System.Windows.Forms.TextBox txtCommands;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Windows.Forms;
+using Renci.SshNet;
+using System.Threading;
+
+namespace SSH_Window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            string[] switchIPs = txtSwitchIPs.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string enablePassword = txtEnablePassword.Text;
+            string[] commands = txtCommands.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            foreach (var ip in switchIPs)
+            {
+                ExecuteCommandsOnSwitch(ip, username, password, enablePassword, commands);
+            }
+        }
+
+        private void ExecuteCommandsOnSwitch(string ip, string username, string password, string enablePassword, string[] commands)
+        {
+            try
+            {
+                using (var client = new SshClient(ip, username, password))
+                {
+                    client.Connect();
+
+                    if (client.IsConnected)
+                    {
+                        AppendOutput($"Connected to switch {ip}.");
+
+                        using (var shellStream = client.CreateShellStream("xterm", 80, 24, 800, 600, 1024))
+                        {
+                            WriteToShellStream(shellStream, "enable");
+                            Thread.Sleep(2000);
+                            var response = ReadFromShellStream(shellStream);
+                            AppendOutput(response);
+
+                            WriteToShellStream(shellStream, enablePassword);
+                            Thread.Sleep(2000);
+                            response = ReadFromShellStream(shellStream);
+                            AppendOutput(response);
+
+                            foreach (var command in commands)
+                            {
+                                WriteToShellStream(shellStream, command);
+                                Thread.Sleep(2000);
+                                response = ReadFromShellStream(shellStream);
+                                AppendOutput($"Command: {command}\nResult: {response}\n");
+                            }
+                        }
+
+                        AppendOutput("Finished executing commands.");
+                    }
+                    else
+                    {
+                        AppendOutput($"Failed to connect to switch {ip}.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendOutput($"An error occurred with switch {ip}: {ex.Message}");
+            }
+        }
+
+        private void WriteToShellStream(ShellStream stream, string command)
+        {
+            stream.WriteLine(command);
+            stream.Flush();
+        }
+
+        private string ReadFromShellStream(ShellStream stream)
+        {
+            string response = string.Empty;
+            while (stream.DataAvailable)
+            {
+                response += stream.Read();
+            }
+            return response;
+        }
+
+        private void AppendOutput(string text)
+        {
+            txtOutput.AppendText(text + Environment.NewLine);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
